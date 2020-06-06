9e661e37ac693960967d9e206a3fd667a3e316df
藤平燎 <fujihiraryo@team-lab.com>
2020-06-07 01:26:10 +0900

    not use Autowired

--- src/main/java/com/example/demo/DemoApiController.java
+++ src/main/java/com/example/demo/DemoApiController.java
@@ -1,18 +1,20 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+
 import java.util.List;
 
 @RestController
 @RequestMapping("demo/api")
+@RequiredArgsConstructor
 public class DemoApiController {
-    @Autowired
+
     private DemoRepository demoRepository;
 
     @GetMapping
--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,17 +1,18 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.RequiredArgsConstructor;
+
 @Controller
 @RequestMapping("/demo")
+@RequiredArgsConstructor
 public class DemoController {
-    @Autowired
-    private DemoRepository demoRepository;
+    private final DemoRepository demoRepository;
 
     @GetMapping
     public String findAll(Model model) {
