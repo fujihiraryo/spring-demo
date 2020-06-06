d9802985f50d6232368301896672cb0aa175ef15
藤平燎 <fujihiraryo@team-lab.com>
2020-06-07 01:30:59 +0900

    不要な部分を削除

--- src/main/java/com/example/demo/DemoEntity.java
+++ src/main/java/com/example/demo/DemoEntity.java
@@ -1,11 +1,7 @@
 package com.example.demo;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -13,17 +9,9 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-@Table(name = "demo_table")
 public class DemoEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
     private int id;
-
-    @Column(name = "name")
     private String name;
-
-    @Column(name = "exist")
     private Boolean exist;
-
 }
