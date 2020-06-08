6ecb7e772eb8be18d77e5d5594fe00556602ae0a
藤平燎 <fujihiraryo@team-lab.com>
2020-06-09 06:44:52 +0900

    repositoryにメソッド追加

--- src/main/java/com/example/demo/DemoRepository.java
+++ src/main/java/com/example/demo/DemoRepository.java
@@ -1,6 +1,11 @@
 package com.example.demo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DemoRepository extends JpaRepository<DemoEntity, Integer> {
+
+    List<DemoEntity> findByNameContaining(String keyword);
+
 }
