6001fc3dd16efe707d77c293721cade7aea13f4c
藤平燎 <fujihiraryo@team-lab.com>
2020-06-08 14:12:10 +0900

    テーブル自動生成ができてない

--- src/main/java/com/example/demo/DemoEntity.java
+++ src/main/java/com/example/demo/DemoEntity.java
@@ -2,16 +2,18 @@ package com.example.demo;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
+@Table(name = "demo_table")
 @Getter
 @Setter
 public class DemoEntity {
     @Id
     private int id;
     private String name;
-    private Boolean exist;
+    private boolean exist;
 }
